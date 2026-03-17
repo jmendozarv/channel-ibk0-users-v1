88d6561214377aad106857e44300fad819f7c25f
jmendozarv <52211870+jmendozarv@users.noreply.github.com>
2026-03-17 15:24:27 -0500

    initial commit

---
+++ src/test/java/com/challenge/ibk/channel_ibk0_users_v1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.ibk.channel_ibk0_users_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
